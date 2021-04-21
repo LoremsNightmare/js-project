5ece7569e1e38e32dceaf83b2419846a9f534e60
LoremsNightmare <nayeem.x7@gmail.com>
2021-04-22 01:56:09 +0600

    academind section 2 classes

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
-console.log(`hello`);
+
+
+
+// class Human{
+//   constructor(){
+//     this.gender = 'male';
+//   }
+//   printGender(){
+//     console.log(this.gender);
+//   }
+// }
+
+
+// class Person extends Human{
+//   constructor(){
+//     super()
+//     this.name = 'olodama';
+//   }
+//   printName(){
+//     console.log(this.name);
+//   }
+// }
+
+
+// const person = new Person
+
+// person.printName()
+// person.printGender()
+
+
+
+
+
+
+
+
+
+class Human{
+  gender = 'male';
+ 
+ printGender = () => {
+   console.log(this.gender);
+ }
+}
+
+
+class Person extends Human{
+ name = 'olodama';
+ 
+ printName = () => {
+   console.log(this.name);
+ }
+}
+
+
+const person = new Person
+
+person.printName()
+person.printGender()
+
+
+
+
+
+
+
+
+
+
+
